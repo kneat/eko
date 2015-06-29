2259fdfd08d1154b6e2b164ea747122dc713d0fb
Trevor Power <trevor_power@yahoo.com>
2015-06-29 12:23:07 +0100

    Accept URL encoded data and output in color

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var colors = require('colors');
+
+colors.setTheme({
+	fatal: 'magenta',
+	error: 'red',
+	warn: 'yellow',
+	info: 'cyan',
+	debug: 'white',
+	trace: 'grey'
+
+});
 
 var app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.post('/', function (req, res) {
-  console.log(req.body);
+  formatter = colors[req.body.level.toLowerCase()];
+  console.log(formatter('%s|%s'), req.body.logger, req.body.message);
   res.status(204).end();
 });
 
 var server = app.listen(3000, function () {
-
   var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+  console.log('eko now listening at http://%s:%s', host, port);
 });
