9d6e506c94c2b29f3b8f060dcdf156e1839579d0
Trevor Power <trevor_power@yahoo.com>
2015-06-29 23:00:49 +0100

    move all console output code together away from server code

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+
 var colors = require('colors');
 
 colors.setTheme({
@@ -9,17 +10,21 @@ colors.setTheme({
 	info: 'cyan',
 	debug: 'white',
 	trace: 'grey'
-
 });
+
+function echoToConsole(req, res, next)
+{
+  var formatter = colors[req.body.level.toLowerCase()];
+  console.log(formatter('%s|%s'), req.body.logger, req.body.message);
+  next();
+}
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.post('/', function (req, res) {
-  formatter = colors[req.body.level.toLowerCase()];
-  console.log(formatter('%s|%s'), req.body.logger, req.body.message);
+app.post('/', echoToConsole, function (req, res) {
   res.status(204).end();
 });
 
