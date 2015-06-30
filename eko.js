ccea68923a6dea840395b136bf01792d9dcb419d
Trevor Power <trevor_power@yahoo.com>
2015-06-30 23:06:32 +0100

    seperated command line code from server code

---
+++ eko.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+var program = require('commander');
+var server = require('./server');
+
+program
+  .version(require('./package.json').version)
+  .description('Repeat log events recieved over HTTP on the console.')
+  .option('-p, --port <num>', 'port to listen on, default is 4560', 4560)
+  .parse(process.argv);
+
+server(program.port);
--- server.js
+++ server.js
@@ -1,26 +1,21 @@
-#!/usr/bin/env node
-var program = require('commander')
 var express = require('express');
 var bodyParser = require('body-parser');
 var echoToConsole = require('./console')
 
-program
-  .version('0.0.1')
-  .description('Repeat log events recieved over HTTP on the console.')
-  .option('-p, --port <num>', 'port to listen on, default is 4560', 4560)
-  .parse(process.argv);
+module.exports = function (port){
+	var app = express();
 
-var app = express();
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+	app.post('/', echoToConsole, function (req, res) {
+	  res.status(204).end();
+	});
 
-app.post('/', echoToConsole, function (req, res) {
-  res.status(204).end();
-});
+	var server = app.listen(port, function () {
+	  var host = server.address().address;
+	  var port = server.address().port;
+	  console.log('eko now listening at http://%s:%s', host, port);
+	});
+}
 
-var server = app.listen(program.port, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('eko now listening at http://%s:%s', host, port);
-});
