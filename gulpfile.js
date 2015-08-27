d2d4dc34972ac52a6a4c28d2f8d47e4b9a11f075
Trevor Power <trevor_power@yahoo.com>
2015-08-27 22:29:22 +0100

    include wiredep css in page

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,37 @@
 var gulp = require('gulp');
 var del = require('del');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify-css');
 var wiredep = require('wiredep');
 
 gulp.task('clean', function(cb){
    del('client', cb);
 });
 
+gulp.task('css:clean', function(cb){
+   del('client/*.css', cb);
+});
+
+gulp.task('css', ['css:clean'], function(){
+   return gulp.src(wiredep().css)
+   .pipe(concat('core.css'))
+   .pipe(minify())
+   .pipe(gulp.dest('client'));   
+});
+
 gulp.task('js:clean', function(cb){
    del('client/*.js', cb);
 });
 
-gulp.task('js', ['js:clean'], function(cb){
+gulp.task('js', ['js:clean'], function(){
    return gulp.src(wiredep().js)
    .pipe(concat('core.js'))
+   .pipe(uglify())
    .pipe(gulp.dest('client'));   
 });
 
-gulp.task('html', ['html:clean'], function(cb){
+gulp.task('html', ['html:clean'], function(){
    return gulp.src('src/*.html')
    .pipe(gulp.dest('client'));
 });
@@ -35,4 +49,4 @@ gulp.task('watch', ['default'], function(){
    gulp.watch('src/*', ['html']);
 });
 
-gulp.task('default', ['js', 'html']);
+gulp.task('default', ['js', 'css', 'html']);
