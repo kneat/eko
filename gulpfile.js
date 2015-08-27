b9bd009271cf256ed30f058a11d3375f0df9b8a2
Trevor Power <trevor_power@yahoo.com>
2015-08-27 21:41:23 +0100

    use wiredep to link to bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,28 @@
 var gulp = require('gulp');
 var del = require('del');
+var wiredep = require('wiredep').stream;
 
 gulp.task('clean', function(cb){
    del('client', cb);
+});
+
+gulp.task('js:clean', function(cb){
+   del('client/*.js', cb);
+});
+
+gulp.task('html', ['html:clean'], function(cb){
+   return gulp.src('src/*.html')
+   .pipe(gulp.dest('client'));
 });
 
 gulp.task('html:clean', function(cb){
    del('client/*.html', cb);
 });
 
-
 gulp.task('html', ['html:clean'], function(cb){
    return gulp.src('src/*.html')
-      .pipe(gulp.dest('client'));
+   .pipe(wiredep())
+   .pipe(gulp.dest('client'));
 });
 
 gulp.task('watch', ['default'], function(){
