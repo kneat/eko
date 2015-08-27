af1a3dbf0de95aca854c8f426b2d284384137ede
Trevor Power <trevor_power@yahoo.com>
2015-08-27 21:31:40 +0100

    added watch task for html generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
 var gulp = require('gulp');
+var del = require('del');
 
-
-gulp.task('clean', function(){
+gulp.task('clean', function(cb){
+   del('client', cb);
 });
 
 gulp.task('html:clean', function(cb){
-   cb();
+   del('client/*.html', cb);
 });
 
 
 gulp.task('html', ['html:clean'], function(cb){
    return gulp.src('src/*.html')
       .pipe(gulp.dest('client'));
+});
+
+gulp.task('watch', ['default'], function(){
+   gulp.watch('src/*', ['html']);
 });
 
 gulp.task('default', ['html']);
